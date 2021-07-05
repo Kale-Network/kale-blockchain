a45998748541206dfe959b6c2ca8fed575281452
kaleseed <kaleseed@github.com>
2021-07-05 13:54:03 -0400

    update kale;

--- kale-blockchain-gui/src/components/core/constants/CurrencyCode.ts
+++ kale-blockchain-gui/src/components/core/constants/CurrencyCode.ts
@@ -2,5 +2,5 @@ import Unit from './Unit';
 import { IS_MAINNET } from './constants';
 
 export default {
-  [Unit.KALE]: IS_MAINNET ? 'XCH' : 'TXCH',
+  [Unit.KALE]: IS_MAINNET ? 'XKA' : 'TXKA',
 };
